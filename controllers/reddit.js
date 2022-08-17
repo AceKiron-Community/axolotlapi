1ebbb1c324c9593f4de50ea11d6daf6900c8d739
Ace <acekiron@outlook.com>
2022-08-17 05:45:36 +0000

    Handling more flairs

--- controllers/reddit.js
+++ controllers/reddit.js
@@ -3,6 +3,8 @@ const Axios = require("axios");
 let showingOff = [];
 let urgentHelp = [];
 let careAdvice = [];
+let artsAndCrafts = [];
+let discussion = [];
 
 async function update() {
     const limit = 100;
@@ -11,27 +13,34 @@ async function update() {
     showingOff = [];
     urgentHelp = [];
     careAdvice = [];
+    artsAndCrafts = [];
+    discussion = [];
 
     for (const child of res.data.data.children) {
-        const { title, score } = child.data;
+        const { title, score, author, total_awards_received } = child.data;
         
         const link = "https://www.reddit.com" + child.data.permalink;
 
-        let images = child.data.media_metadata;
-        if (images) images = Object.values(images).map((media_item) => media_item.s.u.split("?")[0].replace("preview.redd.it", "i.redd.it"));
-        else if (child.data.url) images = [child.data.url];
+        let media = child.data.media_metadata;
+        if (media) media = Object.values(media).map((media_item) => media_item.s.u.split("?")[0].replace("preview.redd.it", "i.redd.it"));
+        else if (child.data.url) media = [child.data.url];
 
-        const payload = { title, score, images, link };
+        let payload = { title, score, media, link, author, total_awards_received };
 
         switch(child.data.link_flair_text) {
             case "Just Showing Off 😍": showingOff.push(payload); break;
             case "Urgent Help": urgentHelp.push(payload); break;
             case "General Care Advice": careAdvice.push(payload); break;
+            case "Arts and Crafts": artsAndCrafts.push(payload); break;
+            
+            case "Discussion": payload.comments = child.data.num_comments; discussion.push(payload); break;
+
+            default: console.log("Unhandled flair: " + child.data.link_flair_text); break;
         }
     }
 
-    const handledLength = showingOff.length + urgentHelp.length + careAdvice.length;
-    console.log(`Handled ${handledLength} out of ${limit} posts, that's ${Math.round(handledLength / limit * 1000) / 10}%!`);
+    const handledLength = showingOff.length + urgentHelp.length + careAdvice.length + artsAndCrafts.length + discussion.length;
+    console.log(`Handled ${handledLength} out of ${limit} posts!`);
 }
 
 module.exports = async ({ router }) => {
@@ -50,5 +59,15 @@ module.exports = async ({ router }) => {
     router.get("/care-advice", (req, res) => {
         if (careAdvice.length == 0) { update(); return res.sendStatus(503); }
         res.send(careAdvice[Math.floor(Math.random() * careAdvice.length)]);
+    });
+
+    router.get("/arts-and-crafts", (req, res) => {
+        if (artsAndCrafts.length == 0) { update(); return res.sendStatus(503); }
+        res.send(artsAndCrafts[Math.floor(Math.random() * artsAndCrafts.length)]);
+    });
+
+    router.get("/discussion", (req, res) => {
+        if (discussion.length == 0) { update(); return res.sendStatus(503); }
+        res.send(discussion[Math.floor(Math.random() * discussion.length)]);
     });
 }
