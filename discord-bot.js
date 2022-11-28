ba5e8b38f7b4af96f0bc67e1e14d05815068e3fd
Ace <acekiron@outlook.com>
2022-11-28 18:53:23 +0100

    Update discord-bot.js

--- discord-bot.js
+++ discord-bot.js
@@ -2,24 +2,36 @@ const { Client, GatewayIntentBits } = require("discord.js");
 const Axios = require("axios");
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
 });
 
 client.on("ready", () => {
-  console.log(`Logged in on Discord as ${client.user.tag}!`);
+    console.log(`Logged in on Discord as ${client.user.tag}!`);
 });
 
 client.on("messageCreate", async (msg) => {
     if (msg.content === "?axolotl") {
-        const post = await Axios.get("https://AxolotlAPI-test.kirondevcoder.repl.co/reddit?minMedia=1&flair=Just%20Showing%20Off%20😍");
+        let post;
+        
+        if (msg.channel.nsfw)
+            post = await Axios.get("https://AxolotlAPI-test.kirondevcoder.repl.co/reddit?minMedia=1&flair=Just%20Showing%20Off%20😍&nsfw=1");
+        else
+            post = await Axios.get("https://AxolotlAPI-test.kirondevcoder.repl.co/reddit?minMedia=1&flair=Just%20Showing%20Off%20😍");
+
         const media = post.data.data[0].media;
         msg.reply(media[Math.floor(Math.random() * media.length)]);
     } else if (msg.content === "?count") {
-        const req = await Axios.get("https://AxolotlAPI-test.kirondevcoder.repl.co/reddit/getcount?minMedia=1&flair=Just%20Showing%20Off%20😍");
+        let req;
+
+        if (msg.channel.nsfw)
+            req = await Axios.get("https://AxolotlAPI-test.kirondevcoder.repl.co/reddit/getcount?minMedia=1&flair=Just%20Showing%20Off%20😍&nsfw=1");
+        else
+            req = await Axios.get("https://AxolotlAPI-test.kirondevcoder.repl.co/reddit/getcount?minMedia=1&flair=Just%20Showing%20Off%20😍");
+
         msg.reply(req.data.data.toString());
     }
 });
